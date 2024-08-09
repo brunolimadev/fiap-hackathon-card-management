1c8ecfb7035c192e24540588507eb94649961f92
Bruno Lima <bruno.lima.dev@outlook.com>
2024-08-08 23:08:45 -0300

    chore: added env

--- src/main/java/br/com/fiap/card_management/adapter/config/PortsConfig.java
+++ src/main/java/br/com/fiap/card_management/adapter/config/PortsConfig.java
@@ -5,23 +5,28 @@ import br.com.fiap.card_management.adapter.ports.outputport.ClientManagementOutp
 import br.com.fiap.card_management.adapter.repositories.CardRepository;
 import br.com.fiap.card_management.ports.outputport.CardManagementOutputPort;
 import br.com.fiap.card_management.ports.outputport.ClientManagementOutputPort;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PortsConfig {
 
+  @Value("${client-management.base-url}")
+  private String clientBaseUrl;
+
   @Bean
   public ClientManagementOutputPort sessionManagementOutputPort() {
 
-    return new ClientManagementOutputPortAdapter();
+    return new ClientManagementOutputPortAdapter(clientBaseUrl);
 
   }
 
   @Bean
   public CardManagementOutputPort itemManagementOutputPort(
-          CardRepository cardRepository,
-          ClientManagementOutputPort clientManagementOutputPort) {
+      CardRepository cardRepository,
+      ClientManagementOutputPort clientManagementOutputPort) {
 
     return new CardManagementOutputPortAdapter(cardRepository, clientManagementOutputPort);
 
--- src/main/java/br/com/fiap/card_management/adapter/ports/outputport/ClientManagementOutputPortAdapter.java
+++ src/main/java/br/com/fiap/card_management/adapter/ports/outputport/ClientManagementOutputPortAdapter.java
@@ -1,12 +1,20 @@
 package br.com.fiap.card_management.adapter.ports.outputport;
 
 import br.com.fiap.card_management.ports.outputport.ClientManagementOutputPort;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class ClientManagementOutputPortAdapter implements ClientManagementOutputPort {
 
-  private final WebClient client = WebClient.create("http://localhost:8082");
+  private final WebClient client;
+
+  public ClientManagementOutputPortAdapter(@Value("${client-management.base-url}") String baseUrl) {
+
+    this.client = WebClient.create(baseUrl);
+
+  }
 
   @Override
   public Object getClient(String cpf) {
